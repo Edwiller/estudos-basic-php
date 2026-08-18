ff235a0e183b5ee032772831638e27d40ed728d4
Ed <edwillerdelima@gmail.com>
2026-08-18 15:21:48 -0500

    postando arquivos desenvolvidos 18.07

--- projetopw/aula1708/cadastrarProduto.php
+++ projetopw/aula1708/cadastrarProduto.php
@@ -8,10 +8,24 @@
     try{
         $conn = new PDO("mysql:host=localhost;dbname=Loja",
                     "root","");
-        var_dump($conn);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "INSERT INTO Produtos(nome,quantidade,preco,data_validade) VALUES (?,?,?,?)";
+
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $quantidade);
+        $stmt->bindParam(3, $preco);
+        $stmt->bindParam(4, $dtValidade);
+
+        $stmt->execute();
+
+        echo "Produto cadastrado com sucesso!";
+        echo "<a href='index.html'>Página inicial<a>";
     }catch(PDOException $erro){
         echo "Erro: Erro na base de dados";
-        //echo $erro->getMessage();
+        echo $erro->getMessage();
     }
 
 
